53a951c2013aafeba922d053971259c7495aa331
bb-t <108349251+bb-t@users.noreply.github.com>
2022-06-28 18:03:21 +0300

    Update updater.py

--- setup/updater.py
+++ setup/updater.py
@@ -3,7 +3,7 @@ import difflib
 import shlex
 from typing import Tuple
 import sys
-//تمت برمجه بواسطه مصطفى
+
 # if any requirements are cahnged then install that requirement
 async def lines_differnce(file1, file2):
     with open(file1) as f1:
